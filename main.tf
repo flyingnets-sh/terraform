44666d122bc0a04ebfdcf8ad556fa754b30f7168
greg <greg>
2023-02-21 07:40:39 +0000

    add var

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-      subscription_id = var.ARM_SUBSCRIPTION_ID
-      client_id       = var.ARM_CLIENT_ID
-      client_secret   = var.ARM_CLIENT_SECRET
-      tenant_id       = var.ARM_TENANT_ID
+      #subscription_id = var.ARM_SUBSCRIPTION_ID
+      #client_id       = var.ARM_CLIENT_ID
+      #client_secret   = var.ARM_CLIENT_SECRET
+      #tenant_id       = var.ARM_TENANT_ID
 }
 
 resource "azurerm_resource_group" "example" {
