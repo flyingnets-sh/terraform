1e85c6b4126668193c3f7fa6a8760e22b78a0e62
greg <greg>
2023-02-21 07:20:12 +0000

    add var

--- main.tf
+++ main.tf
@@ -4,14 +4,16 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
  
+    }
+}
+}
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
       subscription_id = var.ARM_SUBSCRIPTION_ID
       client_id       = var.ARM_CLIENT_ID
       client_secret   = var.ARM_CLIENT_SECRET
       tenant_id       = var.ARM_TENANT_ID
-    }
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
 }
 
 resource "azurerm_resource_group" "example" {
